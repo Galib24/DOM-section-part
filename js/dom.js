b051e3294f9d0eb7d34f6f7593784741a550f54d
SQLAdmin <abuyeahia24@gmail.com>
2023-02-09 12:29:34 +0600

    style with function

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 const places = document.getElementsByClassName('important-places');
 for(const place of places){
-    console.log(place.innerText)
+    // console.log(place.innerText)
 }
 
 const fruitTitle = document.getElementById('fruits-title');
@@ -10,6 +10,17 @@ fruitTitle.innerText = 'fruits changed by js';
 const someLi = document.querySelectorAll('.fruits-container li')
 // console.log(someLi);
 for(const li of someLi){
-    console.log(li.innerText);
+    // console.log(li.innerText);
+
+}
+
+// section site operation
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid steelblue'
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'lightgray';
 
 }
