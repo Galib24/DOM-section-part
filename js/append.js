3f3a45b009ad62be18a81e05fc87c627e6007ac2
SQLAdmin <abuyeahia24@gmail.com>
2023-02-09 14:30:09 +0600

    append child added system  in js

---
+++ js/append.js
@@ -0,0 +1,7 @@
+// where to added
+const placeList = document.getElementById('places-list');
+// what tp be added
+const li = document.createElement('li');
+li.innerText = 'pahartoli bon';
+// add the child
+placeList.appendChild(li);
